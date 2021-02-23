0bec95a04d5eb98fe2a47d7d529fc29601d09175
Sylvester Hofstra <hello@slvstr.dev>
2021-02-23 20:43:04 +0100

    Add initial Game class.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,35 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+
+    /**
+     * Create phrases for use in game
+     * @return {array} An array of possible phrases for the game
+     */
+    createPhrases() {
+        return [
+            new Phrase("You must be the change you wish to see in the world"),
+            new Phrase("Life is a divine adventure"),
+            new Phrase("He who has a why to live can bear almost any how"),
+            new Phrase(
+                "History is the version of past events that people have decided to agree upon"
+            ),
+            new Phrase(
+                "Failure is the condiment that gives success its flavor"
+            ),
+        ];
+    }
+
+    /**
+     * Get random phrase from phrases property
+     * @return {Object} Random phrase object for use in game
+     */
+    getRandomPhrase() {
+        const randomNumber = Math.floor(Math.random() * this.phrases.length);
+
+        return this.phrases[randomNumber];
+    }
+}
